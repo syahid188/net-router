d992239669af312e6966fd80e4f039aed8b7a4d8
Syahid Hizbullah <47060012+syahidhzblh@users.noreply.github.com>
2023-09-05 22:54:57 +0700

    Update user-add-10wh.py

--- user-add-10wh.py
+++ user-add-10wh.py
@@ -6,11 +6,11 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ipaddress_1 = "10.187.7.78"#raena
+ipaddress_1 = "ip_address"
 
 
-#raena
-ssh.connect(hostname=ipaddress_1, port=60556, username='syahid', password='syahid#1001#')
+
+ssh.connect(hostname=ipaddress_1, port=, username='user', password='password')
 ssh.exec_command("system script
 add comment=publicip dont-require-permissions=yes name=publicip owner=admin \
     policy=read,write,test source="/tool fetch mode=https http-method=post htt\
