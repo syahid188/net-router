5f9ac4acb34214179043e8b94f3ebc0b940ab227
Syahid Hizbullah <47060012+syahidhzblh@users.noreply.github.com>
2024-01-17 00:11:09 +0700

    Update add_user.py

--- add_user.py
+++ add_user.py
@@ -7,7 +7,8 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
 #List of Ip Address
 ipAddress = [
-    "192.168.0.245"
+    "192.168.0.1",
+    "192.168.0.2"
 ]
 
 # Command yang akan di execute di terminal cli mikrotik
@@ -15,7 +16,7 @@ command = "user add name=syahid password=syahid group=write comment=IT-Syahid"
 
 # Ganti hostname, port, username, password yang sesuai di ssh.connect()
 for ip in ipAddress:
-    ssh.connect(hostname="192.168.88.1", port=22, username="admintest", password="rahasia")
+    ssh.connect(hostname=ip, port=22, username="admintest", password="rahasia")
     transport = ssh.get_transport()
     if transport is not None:
         print("SSH connection successful On IP " +ip+ "\n" )
