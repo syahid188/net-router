8a4afa2e9ebd14b347acd865ffed2de5cf96bfee
syahidhzblh <syahidhizbullah17@gmail.com>
2024-01-20 21:05:05 +0700

    update config if failed connect

--- add_user.py
+++ add_user.py
@@ -7,25 +7,29 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
 #List of Ip Address
 ipAddress = [
-    "192.168.0.1",
-    "192.168.0.2"
+    "192.168.88.1",
+    "192.168.88.2",
+    "192.168.88.3"
 ]
 
 # Command yang akan di execute di terminal cli mikrotik
 command = "user add name=syahid password=syahid group=write comment=IT-Syahid"
 
+
 # Ganti hostname, port, username, password yang sesuai di ssh.connect()
 for ip in ipAddress:
-    ssh.connect(hostname=ip, port=22, username="admintest", password="rahasia")
-    transport = ssh.get_transport()
-    if transport is not None:
-        print("SSH connection successful On IP " +ip+ "\n" )
-    else:
-        print("SSH connection failed.\n")
-    time.sleep(2)
     try:
+        ssh.connect(hostname=ip, port=22, username="admintest", password="rahasia",timeout=5)
+        print("SSH connection successful On IP " +ip+ "\n" )
+        time.sleep(2)
+        #Exec Command
         ssh.exec_command(command)
+        stdin,stdout,stderr = ssh.exec_command("user print")
+        print (stdout.read().decode("ascii").strip("\n"))
+        time.sleep(2)
+    except TimeoutError as e:
+            print(f"Error: {e} connection on IP " +ip)
     except Exception:
         print(Exception)
-    stdin,stdout,stderr = ssh.exec_command("user print")
-    print (stdout.read().decode("ascii").strip("\n"))
+    finally:
+            ssh.close()
